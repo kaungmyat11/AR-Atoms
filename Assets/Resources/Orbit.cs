f5b3cc11aa0f4a6cfc1129a7c17e727da94ab181
Maxtix <mr.kaungmyat2014myanmar@gmail.com>
2019-04-22 20:19:51 +0630

    first commit

---
+++ Assets/Resources/Orbit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+
+
+	public Transform OrbitPivot;   // The transform that this object will orbit around
+	public float OrbitSpeed = 100f;
+	public Vector3 Axe = Vector3.up;
+
+
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.RotateAround(OrbitPivot.position, Axe, OrbitSpeed * Time.deltaTime);
+	}
+
+
+
+}
+
